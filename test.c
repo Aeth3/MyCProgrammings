eb3678e389fae34b56e478f4506cf57556686d53
Aeth3 <bradams163@gmail.com>
2022-06-15 17:48:41 +0800

    update

--- test.c
+++ test.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+int main(){
+    FILE *file;
+    char data[1000];
+    int len;
+    file = fopen("C:\\Users\\ComP-PC\\Desktop\\hello.txt", "r");
+    while(fgets(data, sizeof(data), file)!=NULL){
+        printf("%s", data);
+    }
+    len = strlen(data);
+    printf("\n%d", len);
+    fclose(file);
+    return 0;
+}

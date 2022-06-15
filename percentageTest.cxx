7c9d2417ce7a13baf4a133299fbf6ab67aa889a1
Aeth3 <bradams163@gmail.com>
2022-06-16 00:00:35 +0800

    update

--- percentageTest.cxx
+++ percentageTest.cxx
@@ -2,12 +2,18 @@
 
 int main(){
     float mark, totalMark, percentage;
-
     printf("Enter your mark:");
     scanf("%f", &mark);
     printf("What is the total mark:");
     scanf("%f", &totalMark);
     percentage = mark/totalMark * 100;
-    printf("Your score percentage is: %f", percentage);
+    printf("Your score percentage is: %.2f%", percentage);
+    if (percentage>=0.75*100)
+    {
+        printf("\nYou've passed!");
+    }
+    else{
+        printf("\nYou've failed!");
+    }
     return 0;
 }
